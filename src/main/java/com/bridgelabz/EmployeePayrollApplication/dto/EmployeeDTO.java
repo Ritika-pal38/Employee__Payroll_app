6d153d65130e96400758588037beb9932e121e8a
Ritika-pal38 <palritika294@gmail.com>
2025-03-22 11:22:19 +0530

    [Ritika] Added code for UC6

--- src/main/java/com/bridgelabz/EmployeePayrollApplication/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/EmployeePayrollApplication/dto/EmployeeDTO.java
@@ -3,50 +3,11 @@ package com.bridgelabz.EmployeePayrollApplication.dto;
 
 import lombok.*;
 
-@Getter
-@Setter
-
+@Data // Generates Getters, Setters, equals(), hashCode(), toString()
+@NoArgsConstructor // Generates Default Constructor
+@AllArgsConstructor // Generates Constructor with All Fields
 public class EmployeeDTO {
     private Long id;
     private String name;
     private double salary;
-
-    // Default Constructor
-    public EmployeeDTO() {}
-
-    // Parameterized Constructor
-    public EmployeeDTO(Long id, String name, double salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
-
-    // Getter and Setter for ID
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) { // No error now!
-        this.id = id;
-    }
-
-    // Getter and Setter for Name
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    // Getter and Setter for Salary
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
 }
-
-
